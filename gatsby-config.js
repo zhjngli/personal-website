a423af459878eebf84fdee1045842eba40e3da79
Zhijiang Li <zl256@cornell.edu>
2021-09-03 01:14:14 -0700

    Use / for manifest start_url

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         name: title,
         short_name: `zli`,
         description: description,
-        start_url: url,
+        start_url: `/`,
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
